2d4773f4333d8c2952ef3768535d573ba4f4ca08
Léo-Paul Dagallier <38457679+LPDagallier@users.noreply.github.com>
2023-05-17 15:00:56 -0400

    add SLURM scripts for HybPiper

---
+++ PHYLOGENY_RECONSTRUCTION/SCRIPTS_cluster/hybpiper2_assemble_TEMPLATE.sh
@@ -0,0 +1,155 @@
+#!/bin/bash
+
+############      SLURM CONFIGURATION      ###################
+#SBATCH --job-name=hybpiper2_assemble_no_stitched_example_analysis_01
+#SBATCH --account=<INSERT ACCOUNT e.g.: soltis>
+#SBATCH --qos=<INSERT QUEUE NAME e.g.: soltis>
+#SBATCH --cpus-per-task=8 
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=32gb 
+#SBATCH --time=4-00:00:00
+#SBATCH --mail-user=<INSERT YOUR EMAIL>
+#SBATCH --mail-type=ALL
+#SBATCH --output=slurm-%x-%j.out
+############################################################
+
+echo "JOB CONFIGURATION"
+echo "Job ID: " $SLURM_JOB_ID
+echo "Name of the job: " $SLURM_JOB_NAME
+echo "Node allocated to the job: " $SLURM_JOB_NODELIST
+echo "Number of nodes allocated to the job: " $SLURM_JOB_NUM_NODES
+echo "Number of CPU tasks in this job: " $SLURM_NTASKS
+echo "Directory from which sbatch was invoked: " $SLURM_SUBMIT_DIR
+echo "Temporary folder in which the job runs: " $SLURM_TMPDIR
+
+###################################################
+#### 1. Preparation of files and transfer to cluster
+###################################################
+
+#### Set up PATHS and VARIABLES
+# change depending on what data is being analysed
+# The analyse identifier that will be used as a preffix or suffix in scripts, file names and/or folder names
+# must be unique to THIS run of analysis (that is this combination of samples, reference file, HYbPiper parameters, etc.)
+analysis_ID="example_analysis_01"
+step_ID="hybpiper2_assemble_no_stiched"
+
+# REFERENCE FILE:
+path_to_ref="<base_directory>/DATASETS/PHYLOGENOMICS/target_references"
+reference_fasta_file="target_reference.FAA"
+
+# DATA-RELATED FILES:
+# Prepare (locally) the following files:
+#namelist.txt: contains the sample names that will be analysed
+#input_fastq: contains the copying comands for batch-copy the fastq files
+#files_renaming.txt: contains the renaming comands for batch-rename the fastq files
+#These files have to be in a folder that has the same name as analysis_ID situated in path_to_dir_in:
+path_to_dir_in="<base_directory>/DATA_ANALYSES/PHYLOGENY_RECONSTRUCTION/DATA";
+
+# OUTPUT FOLDER
+# where the ouptputs will be stored
+path_to_dir_out="<base_directory>/DATA_ANALYSES/PHYLOGENY_RECONSTRUCTION/JOBS_OUTPUTS/"$analysis_ID"_"$step_ID"_"$SLURM_JOB_ID/;
+
+#temporary folder (intermediate files)
+#make temporary directory to store files/run analyses in
+# this will be automatically deleted at the end
+path_to_tmp=$SLURM_TMPDIR
+
+cd $path_to_tmp
+
+module load parallel
+
+# COPY FILES
+echo "COPYING FILES";
+
+echo "copying fasta reference"
+scp $path_to_ref/$reference_fasta_file $path_to_tmp
+echo "The reference file used is "$reference_fasta_file
+echo "done copying fasta reference"
+
+echo "copying data-related files"
+scp $path_to_dir_in/$analysis_ID/namelist_$analysis_ID.txt $path_to_tmp
+scp $path_to_dir_in/$analysis_ID/input_fastq.txt $path_to_tmp
+scp $path_to_dir_in/$analysis_ID/files_renaming.txt $path_to_tmp
+# format text files properly (sometimes windows does weird things on txt files, making them unix-incompatible)
+dos2unix *.txt
+echo "done copying data-related files"
+
+echo "copying fastqs";
+# copy one by one
+# sh input_fastq.txt 
+# parrallel copy using txt file
+# !!! ADJUST THE -j parameter according to the --cpus-per-task SLURM option !!!
+parallel -j 8 < input_fastq.txt 
+
+echo "done copying fastqs";
+echo "DONE COPYING ALL FILES";
+
+# gunzip fastqs
+# in parrallel :
+# !!! ADJUST THE -j parameter according to the --cpus-per-task SLURM option !!!
+echo "gunzipping files"
+ls *.gz | parallel -j 8 gunzip 
+echo "done gunzipping files"
+
+#Renames files in order to have file names corresponding to the list in namelist.txt
+echo "renaming files"
+sh files_renaming2.txt
+echo "done renaming files"
+
+#list the file in the log file to allow checks
+echo "LIST OF THE FILES:"
+ls
+
+# load module HybPiper
+module load hybpiper/2.1.1
+
+###################################################
+#### 2. HybPiper assemble & intronerate
+###################################################
+cd $path_to_tmp
+
+echo "Starting HybPiper assemble and intronerate";
+
+mv $path_to_tmp/namelist_$analysis_ID".txt" $path_to_tmp/namelist.txt
+
+while read name;
+do hybpiper assemble -t_aa $reference_fasta_file -r $name*.fastq --prefix $name --cpu 8 --diamond --run_intronerate --no_stitched_contig;
+done < namelist.txt
+
+echo "Done HybPiper assemble and intronerate";
+
+###################################################
+#### 3. Summary statistics & visualizing results
+###################################################
+
+echo "Starting computing summary statistics for GENES";
+hybpiper stats -t_aa $reference_fasta_file --seq_lengths_filename genes_sequences_lengths --stats_filename hybpiper_genes_statistics gene namelist.txt
+echo "Done computing summary statistics";
+
+echo "Starting computing summary statistics for SUPERCONTIGS";
+hybpiper stats -t_aa $reference_fasta_file --seq_lengths_filename supercontigs_sequences_lengths --stats_filename hybpiper_supercontigs_statistics supercontig namelist.txt
+echo "Done computing summary statistics";
+
+echo "Starting visualizing results with HybPiper script - might lead to bad figure";
+hybpiper recovery_heatmap --heatmap_dpi 300 --heatmap_filetype pdf --heatmap_filename recovery_heatmap_exons genes_sequences_lengths.tsv
+hybpiper recovery_heatmap --heatmap_dpi 300 --heatmap_filetype pdf --heatmap_filename recovery_heatmap_supercontigs supercontigs_sequences_lengths.tsv
+echo "Done visualizing results";
+
+##################################################
+#### 4. Transfer
+##################################################
+
+echo "Transfert data node -> blue storage";
+
+# make output folder in home directory
+mkdir $path_to_dir_out
+
+#fastq files can take up a lot of space so you may want to remove them
+rm $path_to_tmp/*.fastq
+
+# copy everything
+scp -rp $path_to_tmp/* $path_to_dir_out/
+
+echo "done moving, FINISHED";
+
+# All the data are automatically deleted on the node by SLURM
--- PHYLOGENY_RECONSTRUCTION/SCRIPTS_cluster/hybpiper2_extract_TEMPLATE.sh
+++ PHYLOGENY_RECONSTRUCTION/SCRIPTS_cluster/hybpiper2_extract_TEMPLATE.sh
@@ -1,19 +1,35 @@
 #!/bin/bash
-#################################################################
-############                             ########################
-############      HYBPIPER EXTRACT       ########################
-############                             ########################
-#################################################################
+
+############      SLURM CONFIGURATION      ###################
+#SBATCH --job-name=hybpiper2_assemble_no_stitched_example_analysis_01
+#SBATCH --account=<INSERT ACCOUNT e.g.: soltis>
+#SBATCH --qos=<INSERT QUEUE NAME e.g.: soltis>
+#SBATCH --cpus-per-task=8 
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=32gb 
+#SBATCH --time=5-00:00:00
+#SBATCH --mail-user=<INSERT YOUR EMAIL>
+#SBATCH --mail-type=ALL
+#SBATCH --output=slurm-%x-%j.out
+############################################################
+
+echo "JOB CONFIGURATION"
+echo "Job ID: " $SLURM_JOB_ID
+echo "Name of the job: " $SLURM_JOB_NAME
+echo "Node allocated to the job: " $SLURM_JOB_NODELIST
+echo "Number of nodes allocated to the job: " $SLURM_JOB_NUM_NODES
+echo "Number of CPU tasks in this job: " $SLURM_NTASKS
+echo "Directory from which sbatch was invoked: " $SLURM_SUBMIT_DIR
+echo "Temporary folder in which the job runs: " $SLURM_TMPDIR
 
 ###################################################
-#### 1. Preparation (choose 1.1. OR 1.2)
+#### 1. Preparation (choose 1.1 OR 1.2)
 ###################################################
 ###################################################
 ######## 1.1. In cases the list of sample is the SAME as in the previous assembly step
 ###################################################
-#### Set up PATHS and VARIABLES
 analysis_ID="example_analysis_01"
-path_to_assemble="<base_directory>/DATA_ANALYSES/PHYLOGENY_RECONSTRUCTION/JOBS_OUTPUTS/"$analysis_ID"_hybpiper2_assemble/";
+path_to_assemble="<base_directory>/DATA_ANALYSES/PHYLOGENY_RECONSTRUCTION/JOBS_OUTPUTS/"$analysis_ID"_hybpiper2_assemble_no_stitched_<previous_job_ID>/";
 path_to_tmp=$path_to_assemble
 reference_fasta_file="target_reference.FAA"
 
@@ -22,17 +38,18 @@ reference_fasta_file="target_reference.FAA"
 ###################################################
 #### 1.2.1 Set up PATHS and VARIABLES
 analysis_ID="<new_analysis_id>"
+step_ID="hybpiper2_extract"
+
 path_to_dir_in="<base_directory>/DATA_ANALYSES/PHYLOGENY_RECONSTRUCTION/DATA"
 
 path_to_ref="<base_directory>/DATASETS/PHYLOGENOMICS/target_references"
 reference_fasta_file="target_reference.FAA"
 
-step_ID="hybpiper2_extract_no_stiched"
-path_to_dir_out="<base_directory>/DATA_ANALYSES/PHYLOGENY_RECONSTRUCTION/JOBS_OUTPUTS/"$analysis_ID"_"$step_ID/;
+# OUTPUT FOLDER
+# where the ouptputs will be stored
+path_to_dir_out="<base_directory>/DATA_ANALYSES/PHYLOGENY_RECONSTRUCTION/JOBS_OUTPUTS/"$analysis_ID"_"$step_ID"_"$SLURM_JOB_ID/;
 
-path_to_tmp=$path_to_dir_out # if working locally
-mkdir $path_to_tmp
-# path_to_tmp=$SLURM_TMPDIR # if working on a SLURM cluster (in case the variable $SLURM_TMPDIR is not automatically defined by the SLURM manager, the temporary directory would usually have to be on the /scratch directory, see with the cluster's user instructions)
+path_to_tmp=$SLURM_TMPDIR
 
 #### 1.2.2 COPY all the FILES in the working directory
 cd $path_to_tmp
@@ -41,16 +58,14 @@ scp $path_to_dir_in/$analysis_ID/namelist_$analysis_ID".txt" $path_to_tmp
 mv $path_to_tmp/namelist_$analysis_ID".txt" $path_to_tmp/namelist.txt
 scp $path_to_dir_in/$analysis_ID/input_assemblies.txt $path_to_tmp
 dos2unix *.txt
-
-# Copy the assemblies
-## one by one:
-# sh input_assemblies.txt 
-## in parrallel :
-## !!! ADJUST THE -j parameter according to the number of CPUs you want to be used !!!
-parallel -j 4 < input_assemblies.txt 
+# !!! ADJUST THE -j parameter according to the number of CPUs you want to be used !!!
+module load parallel
+echo "copying assemblies"
+parallel -j 8 < input_assemblies.txt 
+echo "done copying assemblies"
 
 #### 1.2.3 [optional] re-compute the assembly statistics
-
+module load hybpiper/2.1.1
 echo "Starting computing summary statistics for GENES";
 hybpiper stats -t_aa $reference_fasta_file --seq_lengths_filename genes_sequences_lengths --stats_filename hybpiper_genes_statistics gene namelist.txt
 echo "Done computing summary statistics";
@@ -64,10 +79,12 @@ hybpiper recovery_heatmap --heatmap_dpi 300 --heatmap_filetype pdf --heatmap_fil
 hybpiper recovery_heatmap --heatmap_dpi 300 --heatmap_filetype pdf --heatmap_filename recovery_heatmap_supercontigs supercontigs_sequences_lengths.tsv
 echo "Done visualizing results";
 
-###################################################
-#### 2. hybpiper retrieve_sequences
-###################################################
+
+####################################################
+#### 2. Retrieve sequences
+####################################################
 cd $path_to_tmp
+module load hybpiper/2.1.1
 
 echo "Retrieve sequences"
 
@@ -86,7 +103,7 @@ hybpiper retrieve_sequences aa -t_aa $reference_fasta_file --sample_names nameli
 echo "Done retrieving sequences"
 
 ####################################################
-##### 3. Post HybPiper file formatting
+#### 3. Post HybPiper file formatting
 ####################################################
 echo "Starting post HybPiper file formatting..."
 
@@ -127,8 +144,18 @@ echo "Done post HybPiper file formatting"
 ##################################################
 #### 4. Transfer to output (ONLY WHEN $path_to_tmp different from $path_to_dir_out)
 ##################################################
+echo "Transfert output from tmp dir to output dir";
 ## if on a cluster: make output folder in home directory
-# mkdir $path_to_dir_out
-# scp -rp $path_to_tmp/* $path_to_dir_out/
-# 
+mkdir $path_to_dir_out
+
+# copy everything (might be long because it also copies the assemblies)
+scp -rp $path_to_tmp/* $path_to_dir_out/
+
+# copy only the retrieved sequences and stat files
+# scp -rp $path_to_tmp/retrieved* $path_to_dir_out/
+# scp -rp $path_to_tmp/*.pdf $path_to_dir_out/
+# scp -rp $path_to_tmp/*.tsv $path_to_dir_out/
+
 echo "FINISHED";
+
+# All the data are automatically deleted on the scratch by SLURM
